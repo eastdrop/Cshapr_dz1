01b4fd97732006c21d65345bfad246c40e3a91af
eastdrop <lyokha7777@gmail.com>
2023-02-14 10:30:43 +0300

    made 2nd task

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else Console.WriteLine("Числа равны");
