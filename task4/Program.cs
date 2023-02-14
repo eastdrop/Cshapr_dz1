1906f64a7e51e5f0b82bd92859afcc552610c6b6
eastdrop <lyokha7777@gmail.com>
2023-02-14 10:40:01 +0300

    made 4th task

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Input number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+
+if (c > max) max = c;
+
+Console.WriteLine(max);
